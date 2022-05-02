16304a33f8568a471c27f4507be3c8c1b783aabf
ByeoungJoon Jeon <juni8453@naver.com>
2022-05-02 18:37:28 +0900

    JPA 시작하기

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,32 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            List<Member> result = em.createQuery("Select m from Member As m", Member.class)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println(member.getId());
+                System.out.println(member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
